8a94a51a45ba89e95352481e5c56f45d549cd3a3
Serhii Fast <Serhii.Fast@exclaimer.com>
2020-10-01 18:44:01 +0300

    update

--- src/app/customers/customer.component.ts
+++ src/app/customers/customer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 import { Customer } from './customer';
 
@@ -9,15 +9,36 @@ import { Customer } from './customer';
   styleUrls: ['./customer.component.css']
 })
 export class CustomerComponent implements OnInit {
+  customerForm: FormGroup;
   customer = new Customer();
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.customerForm = this.formBuilder.group(
+      {
+        firstName: ['',[Validators.required, Validators.minLength(3)]],
+        lastName: ['',[Validators.required, Validators.maxLength(50)]],
+        email: ['',[Validators.required, Validators.email]],
+        phone: '',
+        notification: 'email',
+        sendCatalog: true
+      }
+    );
   }
 
-  save(customerForm: NgForm): void {
-    console.log(customerForm.form);
-    console.log('Saved: ' + JSON.stringify(customerForm.value));
+  setNotification(notifyVia: string): void {
+    const phoneControl = this.customerForm.get('phone');
+    if (notifyVia === 'text')
+      phoneControl.setValidators(Validators.required);
+    if (notifyVia === 'email')
+      phoneControl.clearAsyncValidators();
+
+      phoneControl.updateValueAndValidity();
+  }
+
+  save(): void {    
+    console.log(this.customerForm);
+    console.log('Saved: ' + JSON.stringify(this.customerForm.value));
   }
 }
